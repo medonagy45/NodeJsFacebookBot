185481a0dc5ad718e8a63e4620aa295ef8237f42
mohamed nagy <mohamed nagy>
2017-04-22 10:46:51 +0200

    hello world

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.post('/webhook/', function (req, res) {
 				//sendGenericMessage(sender)
 				continue
 			}
-			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+			sendTextMessage(sender,  text.substring(0, 200)+" nagy ")
 		}
 		if (event.postback) {
 			let text = JSON.stringify(event.postback)
